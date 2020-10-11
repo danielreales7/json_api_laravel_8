b41d321dc999a2871c5a85bb506d32dccc9bf95c
Daniel Reales Harana <daniel@MacBook-Pro-de-Daniel.local>
2020-10-11 14:32:53 +0200

    Ordenando recursos

--- app/Http/Controllers/Api/ArticleController.php
+++ app/Http/Controllers/Api/ArticleController.php
@@ -7,12 +7,29 @@ use App\Http\Resources\ArticleCollection;
 use App\Http\Resources\ArticleResource;
 use App\Models\Article;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class ArticleController extends Controller
 {
     public function index()
     {
-        return ArticleCollection::make(Article::all());
+        $sortFields = Str::of(request('sort'))->explode(',');
+        $articleQuery = Article::query();
+
+        foreach ($sortFields as $sortField) {
+            $direction = 'asc';
+            if(Str::of($sortField)->startsWith('-')) {
+                $direction = 'desc';
+                $sortField = Str::of($sortField)->substr(1);
+            }
+            $articleQuery->orderBy($sortField, $direction);
+        }
+
+        if(request()->missing('sort')) {
+            return ArticleCollection::make(Article::all());
+        }
+
+        return ArticleCollection::make($articleQuery->get());
     }
 
     public function show(Article $article)
---
+++ tests/Feature/Articles/SortArticlesTest.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Tests\Feature\Articles;
+
+use App\Models\Article;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\DB;
+use Tests\TestCase;
+
+class SortArticlesTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function it_can_sort_articles_by_title_asc()
+    {
+        Article::factory()->create(['title' => 'C Title']);
+        Article::factory()->create(['title' => 'A Title']);
+        Article::factory()->create(['title' => 'B Title']);
+
+        $url = route('api.v1.articles.index', ['sort' => 'title']);
+
+        $this->getJson($url)->assertSeeInOrder([
+            'A Title',
+            'B Title',
+            'C Title',
+        ]);
+    }
+
+    /** @test */
+    public function it_can_sort_articles_by_title_desc()
+    {
+        Article::factory()->create(['title' => 'C Title']);
+        Article::factory()->create(['title' => 'A Title']);
+        Article::factory()->create(['title' => 'B Title']);
+
+        $url = route('api.v1.articles.index', ['sort' => '-title']);
+
+        $this->getJson($url)->assertSeeInOrder([
+            'C Title',
+            'B Title',
+            'A Title',
+        ]);
+    }
+
+    /** @test */
+    public function it_can_sort_articles_by_title_and_content()
+    {
+        Article::factory()->create([
+            'title' => 'C Title',
+            'content' => 'B content'
+        ]);
+        Article::factory()->create([
+            'title' => 'A Title',
+            'content' => 'C content'
+        ]);
+        Article::factory()->create([
+            'title' => 'B Title',
+            'content' => 'D content'
+        ]);
+
+//        DB::listen(function($db) {
+//            dump($db->sql);
+//        });
+
+        $url = route('api.v1.articles.index').'?sort=title,-content';
+
+        $this->getJson($url)->assertSeeInOrder([
+            'A Title',
+            'B Title',
+            'C Title',
+        ]);
+
+        $url = route('api.v1.articles.index').'?sort=-content,title';
+
+        $this->getJson($url)->assertSeeInOrder([
+            'D content',
+            'C content',
+            'B content',
+        ]);
+    }
+}
